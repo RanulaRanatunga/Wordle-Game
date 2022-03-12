962e22ebb93406f266b2dfe0f5d69aade9b7af09
Ranula Ranatunga <ranulajanathmax@gmail.com>
2022-03-12 09:44:15 +0530

    Initial Commit

---
+++ Wordle_Game/Wordle_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Wordle_GameApp.swift
+//  Wordle_Game
+//
+//  Created by Ranula Ranatunga on 2022-03-12.
+//
+
+import SwiftUI
+
+@main
+struct Wordle_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
